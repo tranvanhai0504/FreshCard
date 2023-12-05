7b35668f0bd36dfdefb75b0b599859c36f688d15
tvh050423@gmail.com <tvh050423@gmail.com>
2023-12-05 13:48:19 +0700

    add home adapter

--- app/src/main/java/com/example/freshcard/fragments/NewTopicFragment.kt
+++ app/src/main/java/com/example/freshcard/fragments/NewTopicFragment.kt
@@ -1,10 +1,11 @@
-package com.example.freshcard
+package com.example.freshcard.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.freshcard.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [NewTopicFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class NewTopicFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_new_topic, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment NewTopicFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            NewTopicFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
