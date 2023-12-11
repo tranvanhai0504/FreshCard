19251373cf847f5a81aa8743341ffd0a3440e0eb
QuangHuy <huyquangvo11.2000@gmail.com>
2023-12-11 18:11:37 +0700

    change fragment to activity

---
+++ app/src/main/java/com/example/freshcard/ChooseTypeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.freshcard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.freshcard.databinding.ActivityChooseTypeBinding
+
+class ChooseTypeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChooseTypeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
