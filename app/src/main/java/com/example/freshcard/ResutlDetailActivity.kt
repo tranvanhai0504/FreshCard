e23285269f8d18a7a823a14830f1a0b9f753dace
tvh050423@gmail.com <tvh050423@gmail.com>
2023-12-15 15:18:45 +0700

    add activity result

---
+++ app/src/main/java/com/example/freshcard/ResutlDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.freshcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResutlDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resutl_detail)
+
+        var listResult = intent.getExtra
+    }
+}
